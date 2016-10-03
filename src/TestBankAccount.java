cb840153a782429277196940642be43d4c3b6ed7
Kiran <mailtokiran99@gmail.com>
2016-10-03 11:01:43 -0400

    added test case
    added test case

--- src/TestBankAccount.java
+++ src/TestBankAccount.java
@@ -6,5 +6,16 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestBankAccount {
+	
+	
+	@Test
+	 public void testTestDebit(){
+		  
+		  BankAccount baccount =  new BankAccount(10);
+		  
+		  double amount = baccount.debit(5);
+		  
+		  Assert.assertEqulas(5.0,amount);
+	  }
 
 }
